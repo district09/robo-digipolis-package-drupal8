fd17d27e27f651c0b0c85f85e11b5934e07ada40
Jelle Sebreghts <sebreghts.jelle@gmail.com>
2018-11-22 16:02:27 +0100

    Make paths to keep configurable.

--- src/PackageDrupal8.php
+++ src/PackageDrupal8.php
@@ -28,6 +28,59 @@ class PackageDrupal8 extends PackageProject
     ];
 
     /**
+     * Paths to directories/files to keep.
+     *
+     * Defaults to sensible paths when using
+     * https://github.com/drupal-composer/drupal-project (including, but not
+     * limited to, 'config', 'web', 'vendor').
+     *
+     * @var array
+     */
+    protected $keepPaths = [
+        'web',
+        'vendor',
+        'config',
+        'RoboFile.php',
+        'properties.yml',
+        'load.environment.php',
+        'composer.json',
+    ];
+
+    /**
+     * Set paths to directories/files to keep.
+     *
+     * Defaults to sensible paths when using
+     * https://github.com/drupal-composer/drupal-project (including, but not
+     * limited to, 'config', 'web', 'vendor').
+     *
+     * @param array $paths
+     *   Paths to keep.
+     *
+     * @return $this
+     */
+    public function keepPaths(array $paths)
+    {
+        $this->keepPaths = $paths;
+
+        return $this;
+    }
+
+    /**
+     * Adds a path to keep during packaging.
+     *
+     * @param string $path
+     *   Path to keep.
+     *
+     * @return $this
+     */
+    public function keepPath($path)
+    {
+        $this->keepPaths[] = $path;
+
+        return $this;
+    }
+
+    /**
      * {@inheritdoc}
      */
     protected function prepareMirrorDir()
@@ -37,7 +90,7 @@ class PackageDrupal8 extends PackageProject
         $folders = new Finder();
         $folders->in($this->tmpDir);
         $folders->depth(0);
-        $folders->notPath('/^(web|vendor|config|RoboFile\.php|properties\.yml|load\.environment\.php|composer\.json)$/');
+        $folders->notPath('/^(' . implode('|', array_map('preg_quote', $this->keepPaths)) . ')$/');
         $folders->ignoreDotFiles(false);
         $this->fs->remove($folders);
 
